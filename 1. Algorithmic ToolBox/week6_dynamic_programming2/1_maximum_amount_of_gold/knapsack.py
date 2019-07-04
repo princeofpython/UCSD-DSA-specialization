bf21c0e1831e6bc885c6248e59389cd2a67c0c63
Adithya Swaroop <47978078+princeofpython@users.noreply.github.com>
2019-07-04 23:43:52 +0530

    week 6

--- 1. Algorithmic ToolBox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ 1. Algorithmic ToolBox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,17 @@
 # Uses python3
 import sys
-
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+import numpy
+def optimal_weight(W, w, n):
+    weights=numpy.zeros((W+1,n+1),dtype=int)
+    for j in range(1,n+1):
+        for weight in range(1,W+1):
+            listt=[weights[weight,j-1]]
+            if w[j-1]<=weight:
+                listt.append(weights[weight-w[j-1],j-1]+w[j-1])
+            weights[weight,j]=max(listt)
+    return(weights[W,n])
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+    print(optimal_weight(W, w, n))
