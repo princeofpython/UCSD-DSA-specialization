253446573b59923ab4b17665573d9995d3f1f0f6
Adithya Swaroop <47978078+princeofpython@users.noreply.github.com>
2019-07-06 14:45:23 +0530

    Update largest_number.py

--- 1. Algorithmic ToolBox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ 1. Algorithmic ToolBox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -2,16 +2,34 @@
 
 import sys
 
-def largest_number(a):
+def largest_number(a,n):
     #write your code here
     res = ""
-    for x in a:
-        res += x
+    while(a):
+        maxi=a[0]
+        maxindex=0
+        for i,each in enumerate(a):
+            if IsGreaterOrEqual_v2(each,maxi):
+                maxi=each
+                maxindex=i
+        res += maxi
+        del a[maxindex]
     return res
+
+def IsGreaterOrEqual(A,B):  #this is wrong. 
+    a=len(A)
+    b=len(B)
+    if a<=b:
+        return A>=B[:a]
+    else:
+        return A[:b]>B 
+def IsGreaterOrEqual_v2(A,B):
+    return int(A+B)>int(B+A)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
+    n=int(data[0])
     a = data[1:]
-    print(largest_number(a))
+    print(largest_number(a,n))
     
