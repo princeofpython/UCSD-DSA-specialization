f15443a1bc4213df45a13cde09cb90c3512c9d0e
Adithya Swaroop <47978078+princeofpython@users.noreply.github.com>
2019-07-09 12:09:26 +0530

    added new assignment

--- 2. Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ 2. Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -4,17 +4,26 @@ import sys
 class StackWithMax():
     def __init__(self):
         self.__stack = []
+        self.auxstack=[]
 
     def Push(self, a):
         self.__stack.append(a)
+        if(self.auxstack):
+            if self.auxstack[-1]<a:
+                self.auxstack.append(a)
+            else:
+                self.auxstack.append(self.auxstack[-1])
+        else:
+            self.auxstack.append(a)
 
     def Pop(self):
         assert(len(self.__stack))
         self.__stack.pop()
+        self.auxstack.pop()
 
     def Max(self):
         assert(len(self.__stack))
-        return max(self.__stack)
+        return (self.auxstack[-1])
 
 
 if __name__ == '__main__':
