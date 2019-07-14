e3bdf1f98e7a1f18800472beb919e27f58693e55
Adithya Swaroop <47978078+princeofpython@users.noreply.github.com>
2019-07-14 20:06:45 +0530

    passed week 6

--- 2. Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ 2. Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -10,38 +10,48 @@ class TreeOrders:
     self.key = [0 for i in range(self.n)]
     self.left = [0 for i in range(self.n)]
     self.right = [0 for i in range(self.n)]
+    self.resultIN = []
+    self.resultPRE = []
+    self.resultPOST = []
     for i in range(self.n):
       [a, b, c] = map(int, sys.stdin.readline().split())
       self.key[i] = a
       self.left[i] = b
       self.right[i] = c
 
-  def inOrder(self):
-    self.result = []
+  def inOrder(self,index=0):
     # Finish the implementation
     # You may need to add a new recursive method to do that
-                
-    return self.result
+    if self.left[index]!=-1:
+      self.inOrder(self.left[index])
+    self.resultIN.append(self.key[index])
+    if self.right[index]!=-1:
+      self.inOrder(self.right[index])
 
-  def preOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
 
-  def postOrder(self):
-    self.result = []
+  def preOrder(self,index=0):
     # Finish the implementation
     # You may need to add a new recursive method to do that
-                
-    return self.result
+    self.resultPRE.append(self.key[index])
+    if self.left[index]!=-1:
+      self.preOrder(self.left[index])
+    if self.right[index]!=-1:
+      self.preOrder(self.right[index])            
+
+  def postOrder(self,index=0):
+    # Finish the implementation
+    # You may need to add a new recursive method to do that
+    if self.left[index]!=-1:
+      self.postOrder(self.left[index])
+    if self.right[index]!=-1:
+      self.postOrder(self.right[index])  
+    self.resultPOST.append(self.key[index])         
 
 def main():
 	tree = TreeOrders()
-	tree.read()
-	print(" ".join(str(x) for x in tree.inOrder()))
-	print(" ".join(str(x) for x in tree.preOrder()))
-	print(" ".join(str(x) for x in tree.postOrder()))
+	tree.read(); tree.inOrder(); tree.preOrder();  tree.postOrder()
+	print(" ".join(str(x) for x in tree.resultIN))
+	print(" ".join(str(x) for x in tree.resultPRE))
+	print(" ".join(str(x) for x in tree.resultPOST))
 
 threading.Thread(target=main).start()
--- 2. Data Structures/week4_binary_search_trees/2_is_bst/is_bst.py
+++ 2. Data Structures/week4_binary_search_trees/2_is_bst/is_bst.py
@@ -3,19 +3,46 @@
 import sys, threading
 
 sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+threading.stack_size(2**27)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+class Tree:
+  def read(self):
+    self.n = int(sys.stdin.readline())
+    self.key = [0 for i in range(self.n)]
+    self.left = [0 for i in range(self.n)]
+    self.right = [0 for i in range(self.n)]
+    for i in range(self.n):
+      [a, b, c] = map(int, sys.stdin.readline().split())
+      self.key[i] = a
+      self.left[i] = b
+      self.right[i] = c
+    return self.n
 
+  def isBinarySearchTree(self,index=0):
+    if self.left[index]==-1 and self.right[index]==-1:
+      return self.key[index],self.key[index],True
+    
+    if self.left[index]!=-1:
+      left=self.isBinarySearchTree(self.left[index])
+      #leftMin=left[0]
+      #leftMax=left[1]
+      #leftbool=left[2]
 
+    if self.right[index]!=-1:
+      right=self.isBinarySearchTree(self.right[index])
+    if self.left[index]==-1:
+      return min(self.key[index],right[0]), right[1], self.key[index]<=right[0] and right[2]
+
+    elif self.right[index]==-1:
+      return left[0], max(self.key[index],left[1]), self.key[index]>left[1] and left[2]
+
+    else:
+      return min(self.key[index],left[0]), max(self.key[index],right[1]), self.key[index]>left[1] and self.key[index]<=right[0] and left[2] and right[2]
+    
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
+  tree = Tree()
+  n=tree.read()
+  if n==0 or tree.isBinarySearchTree()[2]:
     print("CORRECT")
   else:
     print("INCORRECT")
