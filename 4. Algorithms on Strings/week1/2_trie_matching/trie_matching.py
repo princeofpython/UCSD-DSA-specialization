9266f054792074ccd32f33e364eb65953f726ee7
Adithya Swaroop <47978078+princeofpython@users.noreply.github.com>
2019-07-20 20:58:06 +0530

    passed week 1

--- 4. Algorithms on Strings/week1/2_trie_matching/trie_matching.py
+++ 4. Algorithms on Strings/week1/2_trie_matching/trie_matching.py
@@ -7,11 +7,52 @@ class Node:
 	def __init__ (self):
 		self.next = [NA] * 4
 
+def build_trie(patterns):
+    tree = dict()
+    # write your code here
+    node=0
+    tree[node]=dict()
+    for pattern in patterns:
+        currNode=0
+        letter=0
+        length=len(pattern)
+        while letter<length and pattern[letter] in tree[currNode]:
+            currNode=tree[currNode][pattern[letter]]
+            letter+=1
+        while letter<length:
+            node+=1
+            tree[currNode][pattern[letter]]=node
+            tree[node]=dict()
+            currNode=node
+            letter+=1
+    return tree
+
+def PrefixTrieMatching(text, tree):
+	currNode = 0
+	letter = 0
+	length = len(text)
+	alpha = text[letter]
+	while True:
+		if len(tree[currNode])==0:
+			return True
+		elif alpha in tree[currNode]:
+			currNode=tree[currNode][alpha]
+			letter+=1
+			if letter==length and len(tree[currNode]) != 0:
+				return False
+			elif letter<length:
+				alpha=text[letter]
+		else:
+			return False
+
+
 def solve (text, n, patterns):
 	result = []
-
-	// write your code here
-
+	tree = build_trie(patterns)
+	length = len(text)
+	for i in range(length):
+		if PrefixTrieMatching(text[i:], tree):
+			result.append(i)
 	return result
 
 text = sys.stdin.readline ().strip ()
--- 4. Algorithms on Strings/week1/3_trie_matching_extended/trie_matching_extended.py
+++ 4. Algorithms on Strings/week1/3_trie_matching_extended/trie_matching_extended.py
@@ -6,13 +6,53 @@ NA = -1
 class Node:
 	def __init__ (self):
 		self.next = [NA] * 4
-		self.patternEnd = False
+
+def build_trie(patterns):
+    tree = dict()
+    node=0
+    tree[node]=[dict(),False]
+    for pattern in patterns:
+        currNode=0
+        letter=0
+        length=len(pattern)
+        while letter<length and pattern[letter] in tree[currNode][0]:
+            currNode=tree[currNode][0][pattern[letter]]
+            letter+=1
+        while letter<length:
+            node+=1
+            tree[currNode][0][pattern[letter]]=node
+            tree[node]=[dict(),False]
+            currNode=node
+            letter+=1
+        tree[currNode][1]=True
+    return tree 
+def PrefixTrieMatching(text, tree):
+	currNode = 0
+	letter = 0
+	length = len(text)
+	alpha = text[letter]
+	while True:
+		if tree[currNode][1]:
+			return True
+		elif alpha in tree[currNode][0]:
+			currNode=tree[currNode][0][alpha]
+			letter+=1
+			if letter==length and ( tree[currNode][1]==False):
+				return False
+			elif letter<length:
+				alpha=text[letter]
+		else:
+			return False
+
 
 def solve (text, n, patterns):
 	result = []
-
-	// write your code here
-
+	tree= build_trie(patterns)
+	#print(tree)
+	length = len(text)
+	for i in range(length):
+		if PrefixTrieMatching(text[i:], tree ):
+			result.append(i)
 	return result
 
 text = sys.stdin.readline ().strip ()
